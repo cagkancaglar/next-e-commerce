98b3bebf387bbc83eb0f5589b6a132164e855cd2
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-03 09:12:13 +0300

    Add: Form state & validation

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -4,15 +4,63 @@ import React from "react";
 import AuthFormContainer from "@components/AuthFormContainer";
 import { Button, Input } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 export default function Register() {
+  const validationSchema = yup.object().shape({
+    name: yup.string().required("Name is required!"),
+    email: yup.string().email("Invalid email!").required("Email is required!"),
+    password: yup
+      .string()
+      .min(8, "Password must be at least 8 characters.")
+      .required("Password is required!"),
+  });
+
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    errors,
+    touched,
+  } = useFormik({
+    initialValues: { name: "", email: "", password: "" },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const { name, email, password } = values;
+
   const formErrors: string[] = [];
 
   return (
-    <AuthFormContainer title="Create New Account">
-      <Input name="name" label="Name" />
-      <Input name="email" label="Email" />
-      <Input name="password" label="Password" type="password" />
+    <AuthFormContainer title="Create New Account" onSubmit={handleSubmit}>
+      <Input
+        name="name"
+        label="Name"
+        crossOrigin={undefined}
+        value={name}
+        onChange={handleChange}
+      />
+      <Input
+        name="email"
+        label="Email"
+        crossOrigin={undefined}
+        value={email}
+        onChange={handleChange}
+      />
+      <Input
+        name="password"
+        label="Password"
+        type="password"
+        crossOrigin={undefined}
+        value={password}
+        onChange={handleChange}
+      />
       <Button type="submit" className="w-full">
         Register
       </Button>
