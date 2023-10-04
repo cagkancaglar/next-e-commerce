28ce4dbc4b6931d5872327e2628e1aea602746f8
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-04 18:18:25 +0300

    Add: Email verification token

---
+++ app/models/emailVerificationToken.ts
@@ -0,0 +1,67 @@
+import { genSalt, hash, compare } from "bcrypt";
+import { Document, ObjectId, Schema, Model, models, model } from "mongoose";
+
+interface EmailVerificationTokenDocument extends Document {
+  user: ObjectId;
+  token: string;
+  createdAt: Date;
+}
+
+interface Methods {
+  compareToken(token: string): Promise<boolean>;
+}
+
+const EmailVerificationTokenSchema = new Schema<
+  EmailVerificationTokenDocument,
+  {},
+  Methods
+>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 60 * 60 * 24,
+  },
+});
+
+EmailVerificationTokenSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("token")) {
+      return next();
+    }
+
+    const salt = await genSalt(10);
+    this.token = await hash(this.token, salt);
+    next();
+  } catch (err) {
+    throw err;
+  }
+});
+
+EmailVerificationTokenSchema.methods.compareToken = async function (
+  tokenToCompare
+) {
+  try {
+    return await compare(tokenToCompare, this.token);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const EmailVerificationToken =
+  models.EmailVerificationToken ||
+  model("EmailVerificationToken", EmailVerificationTokenSchema);
+
+export default EmailVerificationToken as Model<
+  EmailVerificationTokenDocument,
+  {},
+  Methods
+>;
