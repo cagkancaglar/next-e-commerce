29bd139a455e6c1a54d56359859d0a7823891941
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-05 00:16:39 +0300

    Add: Generating verification token

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,8 @@ import UserModel from "@models/userModel";
 import { NewUserRequest } from "@/app/types";
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import EmailVerificationToken from "@models/emailVerificationToken";
+import crypto from "crypto";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
@@ -10,6 +12,12 @@ export const POST = async (req: Request) => {
   await startDb();
   const newUser = await UserModel.create({
     ...body,
+  });
+
+  const token = crypto.randomBytes(36).toString("hex");
+  await EmailVerificationToken.create({
+    user: newUser._id,
+    token,
   });
 
   const transport = nodemailer.createTransport({
@@ -21,10 +29,12 @@ export const POST = async (req: Request) => {
     },
   });
 
+  const verificationUrl = `http://localhost:3000/verify?token=${token}&userId=${newUser._id}`;
+
   await transport.sendMail({
     from: "verification@nextecom.com",
     to: newUser.email,
-    html: `<h1>Please verify your email by clicking on <a href="http://localhost:3000">this link </a> </h1>`,
+    html: `<h1>Please verify your email by clicking on <a href="${verificationUrl}">this link </a> </h1>`,
   });
 
   return NextResponse.json(newUser);
