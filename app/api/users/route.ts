450c875ec15674aa79eb51e096d0decb4bb21f26
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-04 17:05:00 +0300

    Add: Nodemailer

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,6 +2,7 @@ import startDb from "@lib/db";
 import UserModel from "@models/userModel";
 import { NewUserRequest } from "@/app/types";
 import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
@@ -9,6 +10,21 @@ export const POST = async (req: Request) => {
   await startDb();
   const newUser = await UserModel.create({
     ...body,
+  });
+
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "196b9845497f3c",
+      pass: "812b27a056ad4a",
+    },
+  });
+
+  await transport.sendMail({
+    from: "verification@nextecom.com",
+    to: newUser.email,
+    html: `<h1>Please verify your email by clicking on <a href="http://localhost:3000">this link </a> </h1>`,
   });
 
   return NextResponse.json(newUser);
