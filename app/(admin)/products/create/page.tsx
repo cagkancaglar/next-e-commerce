6ab1d5f78e9d25b44b0e87636a47e6e942b803f0
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-19 00:59:34 +0300

    Add: Using validation schema

--- app/(admin)/products/create/page.tsx
+++ app/(admin)/products/create/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 import { NewProductInfo } from "@/app/types";
+import { newProductInfoSchema } from "@/app/utils/validationSchema";
 import ProductForm from "@components/ProductForm";
 import React from "react";
+import { ValidationError } from "yup";
+import { toast } from "react-toastify";
 
 export default function Create() {
-  const handleCreateProduct = (values: NewProductInfo) => {};
+  const handleCreateProduct = async (values: NewProductInfo) => {
+    try {
+      await newProductInfoSchema.validate(values, { abortEarly: false });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        error.inner.map((err) => {
+          toast.error(err.message);
+        });
+      }
+    }
+  };
 
   return (
     <div>
