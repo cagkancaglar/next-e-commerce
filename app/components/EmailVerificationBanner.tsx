7c6acd60cea4b4aadb8c8ee9715025a6553f927d
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-24 01:44:59 +0300

    Fix: Single quote problem for Vercel

--- app/components/EmailVerificationBanner.tsx
+++ app/components/EmailVerificationBanner.tsx
@@ -29,7 +29,7 @@ export default function EmailVerificationBanner() {
 
   return (
     <div className="p-2 text-center bg-blue-50">
-      <span>It looks like you haven't verified your email.</span>
+      <span>{`It looks like you haven't verified your email.`}</span>
       <button
         onClick={applyForReverification}
         className="ml-2 font-semibold underline"
