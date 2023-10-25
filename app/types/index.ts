6ab1b25743e3dad4f5f012f97abb1e2281dbd1f9
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-26 00:12:52 +0300

    Fix: Cart API

--- app/types/index.ts
+++ app/types/index.ts
@@ -86,3 +86,8 @@ export interface ProductToUpdate {
   thumbnail?: { url: string; id: string };
   images?: { url: string; id: string }[];
 }
+
+export interface NewCartRequest{
+  productId: string,
+  quantity: number
+}
