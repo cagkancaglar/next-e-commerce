d45b73a087040c5234be153b5f9018224f6dbec9
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-27 04:45:46 +0300

    Add: Featured product form

---
+++ app/(admin)/products/featured/add/page.tsx
@@ -0,0 +1,10 @@
+import FeaturedProductForm from "@components/FeaturedProductForm";
+import React from "react";
+
+export default function AddFeaturedProduct() {
+  return (
+    <div>
+      <FeaturedProductForm />
+    </div>
+  );
+}
