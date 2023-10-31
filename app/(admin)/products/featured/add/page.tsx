21934fe67fb7d7a7fb2b0e8f12c07f143beb13aa
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-10-31 11:50:49 +0300

    Add: Rendering featured product

--- app/(admin)/products/featured/add/page.tsx
+++ app/(admin)/products/featured/add/page.tsx
@@ -1,12 +1,31 @@
 import FeaturedProductTable from "@components/FeaturedProductTable";
 import FeaturedProductForm from "@components/FeaturedProductForm";
 import React from "react";
+import startDb from "@lib/db";
+import FeaturedProductModel from "@models/featuredProduct";
 
-export default function AddFeaturedProduct() {
+const fetchFeaturedProducts = async () => {
+  await startDb();
+  const products = await FeaturedProductModel.find();
+
+  return products.map((product) => {
+    return {
+      id: product._id.toString(),
+      title: product.title,
+      link: product.link,
+      linkTitle: product.linkTitle,
+      banner: product.banner.url,
+    };
+  });
+};
+
+export default async function AddFeaturedProduct() {
+  const featuredProducts = await fetchFeaturedProducts();
+
   return (
     <div>
       <FeaturedProductForm />
-      <FeaturedProductTable products={[]} />
+      <FeaturedProductTable products={featuredProducts} />
     </div>
   );
 }
