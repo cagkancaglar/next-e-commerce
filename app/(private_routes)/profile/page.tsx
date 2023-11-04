e38e01befcea6ede2e34c49b07ead5026e4e5703
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-04 18:54:08 +0300

    Add: Profile page

--- app/(private_routes)/profile/page.tsx
+++ app/(private_routes)/profile/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import ProfileForm from "@/app/components/ProfileForm";
+import startDb from "@/app/lib/db";
+import UserModel from "@/app/models/userModel";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import React from "react";
 
-export default function Profile() {
+const fetchUserProfile = async () => {
+  const session = await auth();
+  if (!session) return redirect("/auth/login");
+
+  await startDb();
+  const user = await UserModel.findById(session.user.id);
+  return {
+    id: user?._id.toString(),
+    name: user?.name,
+    email: user?.email,
+    avatar: user?.avatar?.url,
+  };
+};
+
+export default async function Profile() {
+  const profile = await fetchUserProfile();
   return (
-    <div>Profile</div>
-  )
+    <div>
+      <ProfileForm
+        avatar={profile.avatar}
+        email={profile.email}
+        name={profile.name}
+        id={profile.id}
+      />
+    </div>
+  );
 }
