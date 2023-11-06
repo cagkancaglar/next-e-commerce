10d4a4c91d1264dbaaae583b569398386551fb67
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-06 23:35:43 +0300

    Add: Stripe webhook

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripeSecret = process.env.STRIPE_SECRET_KEY!;
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+
+const stripe = new Stripe(stripeSecret, {
+  apiVersion: "2023-10-16",
+});
+
+export const POST = async (req: Request) => {
+  const data = await req.text();
+
+  const signature = req.headers.get("stripe-signature")!;
+
+  let event;
+
+  try {
+    event = await stripe.webhooks.constructEvent(
+      data,
+      signature,
+      webhookSecret
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as any).message },
+      { status: 400 }
+    );
+  }
+
+  if (event.type === "checkout.session.completed") {
+    // create new order
+    // recount our stock
+  }
+
+  return NextResponse.json({});
+};
