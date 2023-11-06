14bf5f8e962c008cf42725b4d40736519ef2b620
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-06 20:22:53 +0300

    Add: Checkout session URL

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,3 +1,4 @@
+import { getCartItems } from "@lib/cartHelper";
 import { auth } from "@/auth";
 import { isValidObjectId } from "mongoose";
 import { NextResponse } from "next/server";
@@ -29,9 +30,46 @@ export const POST = async (req: Request) => {
         { status: 401 }
       );
 
+    // fetching cart details
+    const cartItems = await getCartItems(session.user.id, cartId);
+    if (!cartItems)
+      return NextResponse.json(
+        {
+          error: "Cart not found!",
+        },
+        { status: 404 }
+      );
+
+    const line_items = cartItems.products.map((product) => {
+      return {
+        price_data: {
+          currency: "USD",
+          unit_amount: product.price * 100,
+          product_data: {
+            name: product.title,
+            images: [product.thumbnail],
+          },
+        },
+        quantity: product.qty,
+      };
+    });
+
     // generate payment link and send to frontend app
-    
+    const params: Stripe.Checkout.SessionCreateParams = {
+      mode: "payment",
+      payment_method_types: ["card"],
+      line_items,
+      success_url: process.env.PAYMENT_SUCCESS_URL!,
+      cancel_url: process.env.PAYMENT_CANCEL_URL!,
+      shipping_address_collection: { allowed_countries: ["TR"] },
+    };
+
+    const checkoutSession = await stripe.checkout.sessions.create(params);
+    return NextResponse.json({ url: checkoutSession.url });
   } catch (error) {
-    throw error;
+    return NextResponse.json(
+      { error: "Something went wrong, could not checkout!" },
+      { status: 500 }
+    );
   }
 };
