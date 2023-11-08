7e5f6242c1d32ac5856c06a3e08e9a6b1ca63ff7
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-11-08 17:58:14 +0300

    Add: Review form

--- app/(home_routes)/[...product]/page.tsx
+++ app/(home_routes)/[...product]/page.tsx
@@ -1,7 +1,8 @@
-import ProductView from "@/app/components/ProductView";
-import startDb from "@/app/lib/db";
-import ProductModel from "@/app/models/productModel";
+import ProductView from "@components/ProductView";
+import startDb from "@lib/db";
+import ProductModel from "@models/productModel";
 import { isValidObjectId } from "mongoose";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -50,6 +51,13 @@ export default async function Product({ params }: Props) {
         points={productInfo.bulletPoints}
         images={productImages}
       />
+
+      <div className="py-4">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-semibold mb-2">Review</h1>
+          <Link href={`/add-review/${productInfo.id}`}>Add Review</Link>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/(home_routes)/add-review/[id]/page.tsx
@@ -0,0 +1,10 @@
+import ReviewForm from "@components/ReviewForm";
+import React from "react";
+
+export default function Review() {
+  return (
+    <div>
+      <ReviewForm productId="" />
+    </div>
+  );
+}
